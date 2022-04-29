45e63575b2da5ef23ec914c3788643c0e6d9a1c9
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-04-29 16:19:25 +0200

    add a list with 10 email

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,18 @@
+/* 
+Descrizione:
+Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+*/
+
 const app = new Vue({
 
     el: '#app',
 
     data: {
-        listaEmail: '',
+        listaEmail: [],
 
     },
 
@@ -15,25 +24,23 @@ const app = new Vue({
 
     mounted() {
 
-        const self = this;
 
-        axios
-            .get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(response => {
-
-                console.log(self);
-                console.log(response);
-
-                self.listaEmail = response.data.response
+        for (i = 0; i < 10; i++) {
 
 
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(response => {
 
+                    console.log(this);
+                    console.log(response);
 
+                    this.listaEmail.push(response.data.response)
 
-            })
+                })
+
+        }
 
     }
-
-
 
 })
